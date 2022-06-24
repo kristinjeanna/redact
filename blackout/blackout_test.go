d6d74650d429a78e28165e49b8cb7be44e3d42c9
Kristin Jeanna <67802120+kristinjeanna@users.noreply.github.com>
2022-06-24 14:07:10 -0500

    Add test for String().

--- blackout/blackout_test.go
+++ blackout/blackout_test.go
@@ -27,3 +27,15 @@ func TestRedact(t *testing.T) {
 		})
 	}
 }
+
+func TestString(t *testing.T) {
+	redactor := New("x")
+	stringer := redactor.(fmt.Stringer)
+
+	expected := `{replacement="x"}`
+	got := stringer.String()
+
+	if expected != got {
+		t.Errorf("Expected '%s', but got '%s'", expected, got)
+	}
+}
