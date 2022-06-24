16b79c60cb6fb5c29dee1a516b6f78d9047b0eea
Kristin Jeanna <67802120+kristinjeanna@users.noreply.github.com>
2022-06-24 14:27:44 -0500

    Add doc comment for Redact().

--- redact.go
+++ redact.go
@@ -2,5 +2,6 @@ package redact
 
 // Redactor is the common interface implemented by all redactors.
 type Redactor interface {
+	// Redact redacts the input string and returns the result.
 	Redact(s string) string
 }
