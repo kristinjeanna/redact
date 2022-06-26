e94a3bc9c6834b4149c3abd362afe2e2f6c5235c
Kristin Jeanna <67802120+kristinjeanna@users.noreply.github.com>
2022-06-26 10:15:35 -0500

    Add error return to Redact() signature.

--- redact.go
+++ redact.go
@@ -2,6 +2,7 @@ package redact
 
 // Redactor is the common interface implemented by all redactors.
 type Redactor interface {
+
 	// Redact redacts the input string and returns the result.
-	Redact(s string) string
+	Redact(s string) (string, error)
 }
